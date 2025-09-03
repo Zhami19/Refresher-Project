9ffc3392d842ff23c7a05e47d46d84244e9a66ee
Zhamanta Chavez <zhamantach04@gmail.com>
2025-09-02 21:39:21 -0500

    Progress on Movement and Animations (Input System implemented)

---
+++ Assets/Scripts/Player Movement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // movement values
+    [Header("Movement Values")]
+    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float jumpSpeed = 10.0f;
+
+    // references
+    Rigidbody rb;
+    [SerializeField] Animator animator;
+
+    // variables
+    Vector3 movementVector;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("walkSpeed", movementVector.magnitude);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        movementVector = new Vector3(inputVector.x, 0, inputVector.y);
+
+        animator.transform.forward = movementVector.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(movementVector * moveSpeed, ForceMode.Acceleration);
+    }
+}
